656f606321c41303fe857462f80a704098bc82d7
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2022-11-21 12:02:38 +0000

    Commit

--- ordersystem/ordersystem_api/urls.py
+++ ordersystem/ordersystem_api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from .views import (
     OrderListApiView,
-    TodoDetailApiView
+    OrderDetailApiView
 )
 
 urlpatterns = [
     path('api', OrderListApiView.as_view()),
-    path('api/<int:todo_id>/', TodoDetailApiView.as_view()),
+    path('api/<int:Order_id>/', OrderDetailApiView.as_view()),
 ]
