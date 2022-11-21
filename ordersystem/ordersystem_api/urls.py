de60a0f50a4d5b3a9a3ba333166a4880809092eb
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2022-11-21 12:02:18 +0000

    Mau

--- ordersystem/ordersystem_api/urls.py
+++ ordersystem/ordersystem_api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .views import (
     OrderListApiView,
+    TodoDetailApiView
 )
 
 urlpatterns = [
     path('api', OrderListApiView.as_view()),
+    path('api/<int:todo_id>/', TodoDetailApiView.as_view()),
 ]
--- ordersystem/ordersystem_api/views.py
+++ ordersystem/ordersystem_api/views.py
@@ -34,3 +34,69 @@ class OrderListApiView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class OrderDetailApiView(APIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self, Order_id, user_id):
+        '''
+        Helper method to get the object with given Order_id, and user_id
+        '''
+        try:
+            return Order.objects.get(id=Order_id, user = user_id)
+        except Order.DoesNotExist:
+            return None
+
+    # 3. Retrieve
+    def get(self, request, Order_id, *args, **kwargs):
+        '''
+        Retrieves the Order with given Order_id
+        '''
+        Order_instance = self.get_object(Order_id, request.user.id)
+        if not Order_instance:
+            return Response(
+                {"res": "Object with Order id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = OrderSerializer(Order_instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # 4. Update
+    def put(self, request, Order_id, *args, **kwargs):
+        '''
+        Updates the Order item with given Order_id if exists
+        '''
+        Order_instance = self.get_object(Order_id, request.user.id)
+        if not Order_instance:
+            return Response(
+                {"res": "Object with Order id does not exists"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        data = {
+            'order': request.data.get('order'), 
+            'ready': request.data.get('ready'), 
+            'user': request.user.id
+        }
+        serializer = OrderSerializer(instance = Order_instance, data=data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # 5. Delete
+    def delete(self, request, Order_id, *args, **kwargs):
+        '''
+        Deletes the Order item with given Order_id if exists
+        '''
+        Order_instance = self.get_object(Order_id, request.user.id)
+        if not Order_instance:
+            return Response(
+                {"res": "Object with Order id does not exists"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        Order_instance.delete()
+        return Response(
+            {"res": "Object deleted!"},
+            status=status.HTTP_200_OK
+        )
