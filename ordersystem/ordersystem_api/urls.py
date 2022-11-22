e5ec0213fae0f1f10dfc871e8175ac96ad467d4d
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2022-11-22 09:01:17 +0000

    Added product

--- ordersystem/ordersystem_api/urls.py
+++ ordersystem/ordersystem_api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
 from .views import (
     OrderListApiView,
-    OrderDetailApiView
+    OrderDetailApiView,
+    ProductListApiView,
+    ProductDetailApiView,
+
 )
 
 urlpatterns = [
     path('api', OrderListApiView.as_view()),
     path('api/<int:Order_id>/', OrderDetailApiView.as_view()),
+    path('api/products/', ProductListApiView.as_view()),
+    path('api/product/<int:Product_id>', ProductDetailApiView.as_view())
 ]
