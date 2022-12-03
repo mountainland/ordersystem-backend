7a64e2e671e7cb251323a0b9fe0fd018af24990b
botsarefuture <vuoreol@proton.me>
2022-12-03 11:35:05 +0200

    Changed couple of things

--- ordersystem/ordersystem_api/serializers.py
+++ ordersystem/ordersystem_api/serializers.py
@@ -3,7 +3,7 @@ from .models import Order, Product
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ["order", "ready", "timestamp", "updated", "user"]
+        fields = ["order", "ready", "timestamp", "updated"]
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
