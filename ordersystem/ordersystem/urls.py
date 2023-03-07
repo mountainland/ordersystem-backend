2cfceffdfcb75c955c3a82cfff967b330c4a6194
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2023-03-07 17:08:22 +0000

    Changed to autogenerated api

--- ordersystem/ordersystem/urls.py
+++ ordersystem/ordersystem/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from ordersystem_api import urls as ordersystem_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('ordersystem/', include(ordersystem_urls)),
+    path('ordersystem/', include("api.urls")),
 ]
--- ordersystem/ordersystem_api/views.py
+++
@@ -1,8 +0,0 @@
-from .order_views import OrderListApiView as OLV, OrderDetailApiView as ODV
-from .product_views import ProductListApiView as PLV, ProductDetailApiView as PDV
-
-OrderDetailApiView = ODV
-OrderListApiView = OLV
-
-ProductDetailApiView = PDV
-ProductListApiView = PLV
